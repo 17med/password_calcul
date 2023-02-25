6b8c4d83c7c5b12d3df766e462278234be1b0ffb
ahmed achour <66746240+17med@users.noreply.github.com>
2023-02-26 00:04:55 +0100

    Add files via upload

---
+++ func.py
@@ -0,0 +1,116 @@
+import customtkinter as ctk
+import passwordmeter
+def verif(m,e):
+    
+    a=False
+    A=False
+    nb=False
+    sp=False
+    for i in m:
+        if(ord(i) in range(ord("a"),ord("z")+1)):
+            a=True
+        elif (ord(i) in range(ord("A"),ord("Z")+1)):
+            A=True
+        elif (ord(i) in range(ord("0"),ord("9")+1)):
+            nb=True
+        else:
+            sp=True
+    if(a):
+        e[0].configure(border_width=2,text_color="green")
+    else:
+        e[0].configure(border_width=0,text_color="grey")
+    if(A):
+        e[1].configure(border_width=2,text_color="green")
+    else:
+        e[1].configure(border_width=0,text_color="grey")
+    if(sp):
+        e[2].configure(border_width=2,text_color="green")
+    else:
+        e[2].configure(border_width=0,text_color="grey")
+    if(nb):
+        e[3].configure(border_width=2,text_color="green")
+    else:
+        e[3].configure(border_width=0,text_color="grey")
+def reset(m):
+    m.set("")
+
+    #m.delete(0,last_index=len(m.get()))
+def showorhide(m):
+    print("h")
+    l=m.cget("show")
+    if(l=="*"):
+        m.configure(show="")
+        return "hide"
+    else:
+        m.configure(show="*")
+        return "show"
+def vals(sc,x,x2,m,l):
+    if(m.get()!=""):
+        verif(m.get(),l)
+        c=time(m.get())
+        strength, improvements = passwordmeter.test(m.get())
+
+        change(sc,x2,strength+0.1)
+        #x.configure(text=c[1])
+    else:
+        
+        x2.configure(text="")
+        x.configure(text="")
+        change(sc,x2,0)
+def change(sc,x2,nb):
+
+    sc.set(nb)
+    if(nb==0):
+        x2.configure(text="")
+        sc.configure(progress_color="#4c4c54")
+    elif(nb<0.4):
+        x2.configure(text="bad")
+        sc.configure(progress_color="red")
+    elif(nb>=0.4 and nb<=0.6):
+        x2.configure(text="ehhh good")
+        sc.configure(progress_color="yellow")
+    else:
+        x2.configure(text="strong")
+        sc.configure(progress_color="green")
+
+import math
+
+def time(password):
+    guesses_per_second = 1000000
+    """
+    Calculate the time it would take to guess a password using brute-force attack.
+
+    Parameters:
+    password (str): The password to be guessed.
+    guesses_per_second (int): The number of guesses that can be made per second.
+
+    Returns:
+    A dictionary containing the estimated time in seconds for a brute-force attack using the given guesses per second.
+
+    """
+    # Calculate the number of possible guesses
+    character_set_size = 95  # printable ASCII characters
+    password_length = len(password)
+    guesses = math.pow(character_set_size, password_length)
+
+    # Calculate the time in seconds
+    time_seconds = guesses / guesses_per_second
+
+    # Format the time in a human-readable way
+    time_string = ""
+    if time_seconds < 60:
+        time_string = "{:.2f} seconds".format(time_seconds)
+    elif time_seconds < 3600:
+        time_string = "{:.2f} minutes".format(time_seconds / 60)
+    elif time_seconds < 86400:
+        time_string = "{:.2f} hours".format(time_seconds / 3600)
+    elif time_seconds<15552000:
+        time_string = "{:.2f} days".format(time_seconds / 86400)
+    elif time_seconds<15552000*1000:
+        time_string = "{:.2f} years".format(time_seconds / 15552000)
+    elif time_seconds<15552000*1000*9999:
+         time_string = "{:.2f} thousand years".format(time_seconds / (15552000*1000))
+    else:
+        time_string = "infinit"
+    # Return the time as a dictionary
+    return  time_seconds,time_string
---
+++ main.py
@@ -0,0 +1,65 @@
+import customtkinter as ctk
+import tkinter
+from element import window
+from func import reset,showorhide,change,vals,verif
+ctk.set_default_color_theme("dark-blue")
+show=False
+win=ctk.CTk()
+win.geometry("600x300")
+win.resizable(False,False)
+win.title("password verif")
+p1 = tkinter.PhotoImage(file = 's.png')
+win.iconphoto(False, p1)
+sc = ctk.CTkProgressBar(win ,width=400, height=20)
+sc.place(x=115,y=170)
+c=False
+
+
+v1=ctk.CTkButton(master=win, text="abc",width=40,font=("normal",40),fg_color="transparent",text_color="grey",height=40,border_color="green",border_width=0
+,hover_color="white", state="DISABLED")
+v1.place(x=450, y=200)
+v2=ctk.CTkButton(master=win, text="ABC",width=40,font=("normal",40),fg_color="transparent",text_color="grey",height=40,border_color="green",border_width=0
+,hover_color="white", state="DISABLED")
+v2.place(x=330, y=200)
+v3=ctk.CTkButton(master=win, text="* !@",width=40,font=("normal",40),fg_color="transparent",text_color="grey",height=40,border_color="green",border_width=0
+,hover_color="white", state="DISABLED")
+v3.place(x=220, y=200)
+v4=ctk.CTkButton(master=win, text="123",width=40,font=("normal",40),fg_color="transparent",text_color="grey",height=40,border_color="green",border_width=0
+,hover_color="white", state="DISABLED")
+v4.place(x=100, y=200)
+
+
+
+tt2=ctk.CTkLabel(master=win,text="",text_color="#404040",font=("normal",40))
+tt2.place(x=300,y=220,anchor="center")
+tt=ctk.CTkLabel(master=win,text="",text_color="#404040",font=("normal",25))
+tt.place(x=300,y=83,anchor="center") 
+def callback(var):
+    global sc
+    verif(psw.get(),[v1,v2,v3,v4])
+    vals(sc,tt2,tt,psw,[v1,v2,v3,v4])
+    pass
+    
+   
+def lp():
+    global c,var
+    c=True  
+    psw.set("")
+
+
+psw = ctk.StringVar()
+psw.trace("w", lambda name, index,mode, psw=psw: callback(psw))
+
+inp=ctk.CTkEntry(master=win,textvariable=psw,height=50,width=200,font=("normal",40),show="*")
+inp.place(x=205, y=100)
+bt0=ctk.CTkButton(master=win, text="reset",width=40,font=("normal",40),fg_color="#C0C0C0",text_color="#202020",height=40,border_color="grey"
+,hover_color="white",command=lp).place(x=95, y=100)
+bt1=ctk.CTkButton(master=win, text="show",width=40,font=("normal",40),fg_color="#C0C0C0",text_color="#202020",height=40,border_color="grey"
+,hover_color="white",)
+bt1.place(x=410, y=100)
+bt1.configure(command=lambda :bt1.configure(text=showorhide(inp)))
+# create CTk scrollbar
+
+change(sc,tt2,0)
+
+win.mainloop()
