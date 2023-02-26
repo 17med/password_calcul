99fe5ce8273a4a37765c241696dba23f9e77bd4c
ahmed achour <66746240+17med@users.noreply.github.com>
2023-02-26 12:42:43 +0100

    Delete t1.py

--- t1.py
+++
@@ -1,9 +0,0 @@
-from element import window,label
-import tkinter
-import customtkinter as ctk
-c=window(height=900,width=500,windowborder=1)
-z=label(c,"hi",size=100,xfromcenter=0.5,yfromcenter=0.49,pos="center")
-z2=label(c,"hello world",size=20,padx=50,pady=50,pos="left")
-
-
-c.show()
